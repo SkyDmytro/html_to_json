f75a7008baa4016625e1babe1d15aad026b519ec
Dima <skydmytro@gmail.com>
2024-10-31 15:53:56 +0100

    add Docs

--- html2json.js
+++ html2json.js
@@ -5,13 +5,24 @@ function convertHtml2JsonAndSet() {
   jsonArea.textContent = JSON.stringify(jsonObj, null, 2);
 }
 
+/**
+ * Converts an HTML string into a structured JSON representation.
+ * The function first checks if the input is a valid HTML string.
+ * If valid, it processes the HTML into a JSON object with detailed structure.
+ *
+ * @param {string} htmlText - The HTML text to be converted into JSON.
+ * @returns {Object} A JSON object containing:
+ *   - "Conversion results": an array of JSON objects representing HTML elements and their hierarchy.
+ *   - "Just to show that it is dynamic value (input length)": the length of the input HTML string.
+ *   If the input is not a string or is invalid HTML, returns an error message.
+ */
 function html2json(htmlText) {
   if (typeof htmlText !== "string") {
     return "Expects a string as argument";
   }
 
   if (!isValidHtml(htmlText)) {
-    return "Invalid HTML";
+    return "Invalid HTML (Check Console)";
   }
   const rootNode = getJsonFromHtml(htmlText);
 
@@ -21,6 +32,11 @@ function html2json(htmlText) {
   };
 }
 
+/**
+ * Converts the given HTML string into a JSON object
+ * @param {string} htmlText The HTML text to convert.
+ * @returns {Object} The JSON object representing the HTML structure.
+ */
 function getJsonFromHtml(htmlText) {
   const rootNode = { type: "root", children: [] };
   const nodeStack = [rootNode];
@@ -52,6 +68,13 @@ function getJsonFromHtml(htmlText) {
   return rootNode;
 }
 
+/**
+ * Appends a text node to the children of the given currentNode if the text is not empty.
+ *
+ * @param {string} text - The text content to be added as a node.
+ * @param {Object} currentNode - The current node to which the text node will be appended.
+ * @throws {Error} If currentNode is null.
+ */
 function handleTextContent(text, currentNode) {
   if (currentNode === null) {
     throw new Error("currentNode must be a non-null object");
@@ -63,6 +86,22 @@ function handleTextContent(text, currentNode) {
   }
 }
 
+/**
+ * Creates a new JSON representation of an HTML element.
+ * @param {string} tagName The name of the element, will be lowercased.
+ * @param {string} attributes A string of attributes for the element.
+ * @returns {Object} A JSON representation of the element, with the following
+ *   properties:
+ *   - `type`: The string "element", indicating that the object represents an
+ *     HTML element.
+ *   - `tagName`: The lowercased name of the element.
+ *   - `attributes`: An object with the attribute names as keys and the
+ *     corresponding values as values. If an attribute is a boolean attribute,
+ *     its value in the returned object is an empty string.
+ *   - `children`: An array that should be populated with the child nodes of the
+ *     element.
+ */
+
 function createElementNode(tagName, attributes) {
   return {
     type: "element",
@@ -72,6 +111,16 @@ function createElementNode(tagName, attributes) {
   };
 }
 
+/**
+ * Takes a string of HTML attributes and parses it into an object.
+ * @param {string} attributesString
+ * @returns {Object} An object with the attribute names as keys and the
+ *   corresponding values as values.
+ *
+ *   The function returns an object with the attribute names as keys and the
+ *   corresponding values as values. If an attribute is a boolean attribute, its
+ *   value in the returned object is an empty string.
+ */
 function parseAttributes(attributesString) {
   const attributes = {};
   if (!attributesString || typeof attributesString !== "string") {
@@ -102,7 +151,6 @@ function parseAttributes(attributesString) {
 }
 
 function showExample1() {
-  console.log("showExample1");
   const jsonContent = {
     "Comment 1":
       "You have to think about how to take into account various html inputs so your json structure will cover them all and handle different cases.",
@@ -119,7 +167,6 @@ function showExample1() {
 }
 
 function showExample2() {
-  console.log("showExample2");
   const jsonContent = {
     "Comment 1":
       "You have to think about how to take into account various html inputs so your json structure will cover them all and handle different cases.",
@@ -135,9 +182,27 @@ function showExample2() {
   );
 }
 
+/**
+ * Checks if the given HTML string is valid.
+ * @param {string} html The HTML string to check.
+ * @returns {boolean} true if the HTML is valid, false otherwise.
+ * @throws {Error} if the HTML is invalid.
+ */
 function isValidHtml(html) {
-  return validateTags(html) && basicValidation(html);
+  try {
+    return validateTags(html) && basicValidation(html);
+  } catch (error) {
+    console.error("Error while validating HTML:", error);
+    return false;
+  }
 }
+
+/**
+ * Validates that all tags in the given HTML string are properly closed.
+ *
+ * @throws {Error} if any tags are unclosed
+ * @returns {boolean} true if all tags are closed, false otherwise
+ */
 
 function validateTags(html) {
   const stack = [];
@@ -157,27 +222,56 @@ function validateTags(html) {
       }
     } else {
       if (stack.length === 0 || stack.pop() !== tag) {
-        return false;
+        throw new Error("Unclosed tag");
       }
     }
   }
 
-  return stack.length === 0;
+  if (stack.length > 0) {
+    throw new Error(`Unclosed tags: ${stack.join(", ")}`);
+  }
+
+  return true;
 }
 
+/**
+ * Performs a basic validation check to ensure that the provided HTML string
+ * contains any tags. This is a very basic check and does not validate the
+ * structure or correctness of the HTML tags. If the HTML string does not contain
+ * any tags, an error is thrown.
+ * @param {string} html HTML string to validate
+ * @returns {boolean} true if the HTML contains any tags, false otherwise
+ * @throws {Error} if the HTML does not contain any tags
+ */
 function basicValidation(html) {
   const tagPattern = /<\/?[\w\s="/.':;#-\/\?]+>/gi;
 
-  return html.match(tagPattern).length > 0;
-}
+  if (!html.match(tagPattern)) {
+    throw new Error("No tags found in HTML");
+  }
 
+  return true;
+}
+/**
+ * Runs validation tests on the provided htmlExamples array.
+ * Returns a string indicating success or failure of the tests.
+ * @returns {string} "TESTS PASSED" if all samples pass validation, or
+ * "TESTS FAILED" if any sample fails validation.
+ */
 function test() {
   const results = htmlExamples.map(({ htmlstring, isValid }) => {
     return isValid === isValidHtml(htmlstring);
   });
-  return results.some((it) => it === false) ? "TESTS FAILED" : "TESTS PASSED";
+  return results.some((it) => it === false)
+    ? "TESTS FAILED 😞 (Check console)"
+    : "TESTS PASSED 🎉";
 }
 
+/**
+ * Updates the content of the JSON output textarea with the results of HTML validation tests.
+ * Executes the test function to determine if HTML samples pass validation, and displays
+ * a success or failure message in the output area.
+ */
 function showTestsOutput() {
   const content = test();
   document.getElementById("json").textContent = content;
